b04b26d1e523399e780fa41430f32379fa1a644f
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-07-02 12:29:00 +0100

    Add a basic PUT endpoint that returns a 200 status code
    - This is to check that we've set it up correctly. This isn't going to
      be the final product.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/healthcheck', :to => proc { [200, {}, ['OK']] }
+
+  put '/hmrc-manuals/:manual_slug', :to => proc { [200, {'Content-Type' => 'application/json'}, ['{ "status": "ok" }']] }
 end
