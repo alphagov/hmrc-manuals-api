b30cbb600cf5539383fccc9c5d767dea6760c48e
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-07-02 15:33:51 +0100

    Change more "" to '' for consistency with the rest of the app

--- spec/requests/healthcheck_spec.rb
+++ spec/requests/healthcheck_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-describe "healthcheck path" do
-  it "responds with 'OK'" do
-    get "/healthcheck"
+describe 'healthcheck path' do
+  it 'responds with "OK"' do
+    get '/healthcheck'
     expect(response).to be_success
-    expect(response.body).to eq("OK")
+    expect(response.body).to eq('OK')
   end
 end
