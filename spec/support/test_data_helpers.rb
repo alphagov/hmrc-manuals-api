38afd01417eeea1e45ec685e2c17a017eea8f163
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-07-15 14:29:07 +0000

    Add a schema for manuals
    Updated tests to continue to use valid test data.

--- spec/support/test_data_helpers.rb
+++ spec/support/test_data_helpers.rb
@@ -1,6 +1,13 @@
 module TestDataHelpers
   def valid_manual(options = {})
-    { title: 'Employment Income Manual' }.merge(options)
+    {
+      title: 'Employment Income Manual',
+      description: 'A manual about incoming employment',
+      public_updated_at: '2014-01-23T00:00:00+01:00',
+      details: {
+        child_section_groups: []
+      }
+    }.merge(options)
   end
 end
 
