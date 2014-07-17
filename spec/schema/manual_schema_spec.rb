0149108a60600ee4d6ee2bc1d357874c8d6d0f81
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-07-17 11:03:06 +0100

    Remove workaround for symbol keys in test data
    This now works as expected in the gem.

--- spec/schema/manual_schema_spec.rb
+++ spec/schema/manual_schema_spec.rb
@@ -3,17 +3,7 @@ require 'rails_helper'
 describe 'manual schema' do
   def get_validation_errors(manual, options = {})
     options = { validate_schema: true }.merge(options)
-
-    # Work around an inconsistency in JSON::Validator where some keys in the data
-    # have to be strings, even though it appears to support symbol keys. Do this
-    # by converting the hash to a JSON string and letting JSON::Validator parse
-    # it back.
-    #
-    # This isn't a problem in the controller, because that always receives a JSON
-    # string.
-    #
-    # See: https://github.com/hoxworth/json-schema/issues/104
-    JSON::Validator.fully_validate(MANUAL_SCHEMA, manual.to_json, options)
+    JSON::Validator.fully_validate(MANUAL_SCHEMA, manual, options)
   end
 
   context 'a minimal document' do
