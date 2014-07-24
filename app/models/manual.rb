495c52dd0bea2dd9778d9077ae1be41c58cd7a56
Jake Benilov <benilov@gmail.com>
2014-07-24 14:10:36 +0000

    Refactor manual JSON schema validation
    This introduces a reusable validator and moves JSON schema validation to the model.

---
+++ app/models/manual.rb
@@ -0,0 +1,12 @@
+require 'active_model'
+
+class Manual
+  include ActiveModel::Validations
+
+  attr_reader :manual_attributes
+  validates :manual_attributes, conforms_to_json_schema: { schema: MANUAL_SCHEMA }
+
+  def initialize(manual_attributes)
+    @manual_attributes = manual_attributes
+  end
+end
