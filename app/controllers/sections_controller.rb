509c4ce55a07666fa11bf099cb4c4e4e186922c6
Jake Benilov <benilov@gmail.com>
2014-07-24 14:14:09 +0000

    Refactor section JSON schema validation

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,15 +2,12 @@ class SectionsController < ApplicationController
   before_filter :parse_request_body, only: [:update]
 
   def update
-    validation_errors = JSON::Validator.fully_validate(
-      SECTION_SCHEMA,
-      @parsed_request_body,
-      validate_schema: true
-    )
-    if validation_errors.empty?
+    section = Section.new(@parsed_request_body)
+
+    if section.valid?
       render nothing: true, content_type: "application/json", status: 200
     else
-      render json: { status: "error", errors: validation_errors }, status: 422
+      render json: { status: "error", errors: section.errors.full_messages }, status: 422
     end
   end
 end
