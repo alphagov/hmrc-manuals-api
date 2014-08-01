b7c2ed973f2092a6d3b65aeb9298c352287ddd8b
Jake Benilov <benilov@gmail.com>
2014-08-01 15:23:08 +0000

    Markdown => HTML in 'body' and 'description'
    `kramdown` is used to convert the markdown. However the `govspeak` gem is included
    in the dependency, because HTML sanitisation will use this gem.

---
+++ lib/struct_with_rendered_markdown.rb
@@ -0,0 +1,33 @@
+require 'kramdown'
+
+class StructWithRenderedMarkdown
+  ATTRIBUTES_THAT_CAN_CONTAIN_MARKDOWN = [ "body", "description" ]
+
+  def initialize(struct)
+    @struct = struct.dup
+  end
+
+  def to_h
+    render_markdown_in(@struct)
+  end
+
+private
+  def render_markdown_in(struct)
+    if struct.is_a?(Array)
+      struct.each { |item| render_markdown_in(item) }
+    elsif struct.is_a?(Hash)
+      struct.each do |key, value|
+        if ATTRIBUTES_THAT_CAN_CONTAIN_MARKDOWN.include?(key)
+          struct[key] = markdown_to_html(value)
+        elsif value.is_a?(Hash) or value.is_a?(Array)
+          render_markdown_in(value)
+        end
+      end
+    end
+    struct
+  end
+
+  def markdown_to_html(string)
+    Kramdown::Document.new(string).to_html
+  end
+end
