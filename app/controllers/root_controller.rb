62efbf6b6aec697a0eaa966a45d712d80be43714
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-08-12 13:37:26 +0000

    Rewrite public links in README to work over HTTP
    The README is written for GitHub, where they work as-is, but we want them to
    work when viewing the README in the running app.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -9,8 +9,25 @@ class RootController < ActionController::API
   end
 
   def readme
+    content = load_readme
+    content = rewrite_public_links(content)
+    render :readme, locals: { content: content }, layout: true
+  end
+
+private
+
+  def load_readme
     file_contents = File.open(Rails.root + "README.md", 'r').read
-    markdown = Kramdown::Document.new(file_contents).to_html
-    render :readme, locals: { markdown: markdown }, layout: true
+    content = Kramdown::Document.new(file_contents).to_html
+  end
+
+  def rewrite_public_links(content)
+    doc = Nokogiri::HTML.parse(content)
+    doc.css("a[href^='/public/']", "a[href^='public/']").each do |anchor|
+      # find all links with a path which starts "public" or "/public"
+      new_path = anchor.attributes['href'].value.gsub(%r{^/?public}, '')
+      anchor.attributes['href'].value = new_path
+    end
+    doc.to_html
   end
 end
