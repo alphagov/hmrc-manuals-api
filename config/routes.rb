10d98bf34ea443c4a33b13eb3f97e654b2a90917
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-08-18 16:00:34 +0000

    Support POST on PUT routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
     r.get '/readme', controller: 'root', action: 'readme'
     r.get '/healthcheck', :to => proc { [200, {}, ['OK']] }
 
+    # Support POST for PUT URLs as per:
+    # https://www.gov.uk/service-manual/making-software/apis.html#use-http-methods-as-tim-intended
+    r.post '/hmrc-manuals/:id', controller: "manuals", action: :update
+    r.post '/hmrc-manuals/:manual_id/sections/:id', controller: "sections", action: :update
+
     # We need to override the controller and url helper here because rails is unhappy
     # with the dash in 'hmrc-manuals'.
     r.resources "hmrc-manuals", only: :update, controller: "manuals", as: "manuals" do
--- spec/support/json_request_helpers.rb
+++ spec/support/json_request_helpers.rb
@@ -8,6 +8,15 @@ module JSONRequestHelper
     put path, attrs.to_json, default_headers.merge(headers)
   end
 
+  def post_json(path, attrs, headers = {})
+    default_headers = {
+      "CONTENT_TYPE" => "application/json",
+      'HTTP_ACCEPT' => 'application/json',
+      'HTTP_AUTHORIZATION' => 'Bearer 12345678'
+    }
+    post path, attrs.to_json, default_headers.merge(headers)
+  end
+
   def json_response
     JSON.parse(response.body)
   end
