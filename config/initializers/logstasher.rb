6fa061a1043858bcae0027496f581c6ade1d2b4c
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-12-05 14:53:11 +0000

    Stop logging GOVUK_Request-Id - it's never going to get frontend traffic
    - This was referenced in the Logstasher section of
      https://github.com/alphagov/wiki/wiki/Setting-up-a-new-app#for-rails-apps
      but it is applicable only to frontend apps, which HAPI isn't.

--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -4,8 +4,6 @@ if Object.const_defined?('LogStasher') && LogStasher.enabled
   LogStasher.add_custom_fields do |fields|
     # Mirrors Nginx request logging, e.g GET /path/here HTTP/1.1
     fields[:request] = "#{request.request_method} #{request.fullpath} #{request.headers['SERVER_PROTOCOL']}"
-    # Pass request Id to logging
-    fields[:govuk_request_id] = request.headers['GOVUK-Request-Id']
 
     if HMRCManualsAPI::Application.config.log_request_body
       # request.body is a StringIO and may have already been read, so we need to
