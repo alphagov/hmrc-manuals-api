2b610a13c487541e6c4f87807511bdb07c2d5605
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-07-19 13:51:47 +0100

    Disable session storage
    This application doesn't use cookies we have no need to store the
    session.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hmrc_manuals_api_session'
+Rails.application.config.session_store :disabled
