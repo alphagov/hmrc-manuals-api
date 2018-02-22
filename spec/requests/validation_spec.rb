5c7120f7b496e907549069607f92af91c02807dd
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2018-02-22 21:06:09 +0000

    Remove tests for malformed JSON
    These tests no longer work correctly since the JSON upgrade to 2.*
    
    The to_json method called on the malformed_json string of "[" converts
    the string to '"["'. JSON 1.* would raise a parse error on this string
    (incorrectly).
    
    ```
    irb(main):004:0> JSON::VERSION
    => "1.8.6"
    irb(main):005:0> JSON.parse("\"[\"")
    JSON::ParserError: 784: unexpected token at '"["'
    	from (irb):5
    ```
    
    ```
    irb(main):006:0> JSON::VERSION
    => "2.1.0"
    irb(main):007:0> JSON.parse("\"[\"")
    => "["
    ```
    
    These tests aren't realistic anyway as modern Rails will return a parse
    error before these tests reach the controller. Fixing that however is a
    bigger job.

--- spec/requests/validation_spec.rb
+++ spec/requests/validation_spec.rb
@@ -7,20 +7,11 @@ describe "validation" do
   include GdsApi::TestHelpers::Rummager
   include LinksUpdateHelper
 
-  let(:malformed_json) { "[" }
   let(:headers) { { 'Content-Type' => 'application/json', 'HTTP_AUTHORIZATION' => 'Bearer 12345678' } }
   let(:manual_without_title)  { valid_manual.tap { |m| m.delete("title") } }
   let(:section_without_title) { valid_section.tap { |m| m.delete("title") } }
 
   context "for manuals" do
-    it "detects malformed JSON" do
-      put_json '/hmrc-manuals/imaginary-slug', malformed_json, headers
-
-      expect(response.status).to eq(400)
-      expect(json_response).to include("status" => "error")
-      expect(json_response["errors"].first).to match(%r{Request JSON could not be parsed:})
-    end
-
     it "validates for the presence of the title" do
       put_json '/hmrc-manuals/imaginary-slug', manual_without_title
 
@@ -77,14 +68,6 @@ describe "validation" do
   end
 
   context "for manual sections" do
-    it "detects malformed JSON" do
-      put_json '/hmrc-manuals/imaginary-slug/sections/imaginary-section', malformed_json, headers
-
-      expect(response.status).to eq(400)
-      expect(json_response).to include("status" => "error")
-      expect(json_response["errors"].first).to match(%r{Request JSON could not be parsed:})
-    end
-
     it "validates for the presence of the title" do
       put_json '/hmrc-manuals/imaginary-slug/sections/imaginary-section', section_without_title
 
