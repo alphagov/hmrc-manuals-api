9f76a1ba50e36c409144ed39d7a77a805422c962
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-09-24 13:39:49 +0100

    Fix Style/ConditionalAssignment offences

--- config/initializers/allow_unknown_hmrc_slugs_feature_flag.rb
+++ config/initializers/allow_unknown_hmrc_slugs_feature_flag.rb
@@ -1,5 +1,5 @@
-if Rails.env.production?
-  HMRCManualsAPI::Application.config.allow_unknown_hmrc_manual_slugs = ENV["ALLOW_UNKNOWN_HMRC_MANUAL_SLUGS"].present?
+HMRCManualsAPI::Application.config.allow_unknown_hmrc_manual_slugs = if Rails.env.production?
+  ENV["ALLOW_UNKNOWN_HMRC_MANUAL_SLUGS"].present?
 else
-  HMRCManualsAPI::Application.config.allow_unknown_hmrc_manual_slugs = true
-end
+  true
+                                                                     end
