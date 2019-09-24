2dd527e47fcf745b3dd7b9a89ff6c24bfef8046e
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-09-24 13:44:44 +0100

    Fix Layout/EndAlignment offences

--- config/initializers/frontend_base_url.rb
+++ config/initializers/frontend_base_url.rb
@@ -2,4 +2,4 @@ FRONTEND_BASE_URL = if Rails.env.development?
                       Plek.new.find("manuals-frontend")
                     else
                       Plek.current.website_root
-end
+                    end
