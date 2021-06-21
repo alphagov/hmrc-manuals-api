22da41674dda6a271f1dd03829dee1587cdbf2f6
Murilo Dal Ri <murilo.dalri@digital.cabinet-office.gov.uk>
2021-06-21 14:07:38 +0100

    Upgrade Rails defaults to 6.1
    Rails was upgraded in https://github.com/alphagov/hmrc-manuals-api/pull/569 but `rails app:update` was not run

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,17 @@
 require_relative "boot"
 
 require "rails"
-
+# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
+# require "action_cable/engine"
 require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -18,15 +23,13 @@ module HMRCManualsAPI
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # We need to put this middleware back, after "rails-api" strips it out.
-    # This middleware must be present, otherwise the "request_store" gem, which
-    # is a dependency of "logstasher", falls over. It's not clear whether "request_store"
-    # actually uses this middleware itself, but it references it in the railtie.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.insert_after(Rack::Runtime, Rack::MethodOverride)
   end
 end
