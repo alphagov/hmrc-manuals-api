6a5f8e8b34637d0e96abf1891e1c12d1eee091f3
Bruce Bolt <bruce.bolt@dsit.gov.uk>
2026-07-27 13:57:54 +0100

    Add basic functionality to create asset endpoint
    This uploads the asset to Asset Manager and returns the relevant
    response to the client.
    
    Does not yet cover any error handling and does not deal with draft
    assets.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,9 +1,51 @@
 class AssetsController < ApplicationController
+  before_action :check_content_type_is_multipart
+
   def create
+    asset = {
+      file: asset_params[:file].tempfile,
+    }
+
+    asset_manager_response = Services.asset_manager.create_asset(asset).to_h
+
     respond_to do |format|
       format.json do
-        render json: {}
+        render status: :created, json: {
+          _response_info: {
+            status: "ok",
+          },
+          asset_id: get_asset_id_from_url(asset_manager_response["file_url"]),
+          content_type: asset_manager_response["content_type"],
+          deleted: asset_manager_response["deleted"],
+          draft: asset_manager_response["draft"],
+          file_url: asset_manager_response["file_url"],
+          id: asset_manager_response["id"],
+          name: asset_manager_response["name"],
+          size: asset_manager_response["size"],
+          state: asset_manager_response["state"],
+        }
       end
     end
+  rescue ActionController::UnknownFormat
+    error :not_acceptable, "Invalid Accept header"
+  end
+
+private
+
+  def check_content_type_is_multipart
+    unless request.headers["Content-Type"].match?(/^multipart\/form-data/)
+      error :unsupported_media_type, "Invalid Content-Type header"
+    end
+  end
+
+  def asset_params
+    params.require(:asset).permit(
+      :file,
+      :draft,
+    )
+  end
+
+  def get_asset_id_from_url(url)
+    url[/\/media\/(.*)\//, 1]
   end
 end
--- spec/requests/assets_spec.rb
+++ spec/requests/assets_spec.rb
@@ -2,14 +2,67 @@ require "rails_helper"
 
 describe "assets resource" do
   describe "POST /assets" do
-    subject do
-      post_multipart "/assets", {}
+    let(:asset_manager_response) do
+      {
+        _response_info: {
+          status: "ok",
+        },
+        content_type: "text",
+        deleted: "false",
+        draft: "false",
+        file_url: "http://asset-manager.dev.gov.uk/media/45678/asset.txt",
+        id: "123",
+        name: "asset.txt",
+        size: "12",
+        state: "clean",
+      }
     end
 
-    it "responds with ok" do
-      subject
+    subject do
+      post_multipart "/assets", {
+        asset: {
+          file: fixture_file_upload("asset.txt", "text/plain"),
+        },
+      }
+    end
 
-      expect(response.status).to eq(200)
+    before do
+      allow(Services.asset_manager).to receive(:create_asset).and_return(asset_manager_response.deep_stringify_keys)
+    end
+
+    context "when Asset Manager responds with ok" do
+      it "responds with 201 Created" do
+        subject
+
+        expect(response.status).to eq(201)
+      end
+
+      it "responds with data from asset manager" do
+        subject
+
+        parsed_response = JSON.parse(response.body).deep_symbolize_keys
+        expect(parsed_response).to include(asset_manager_response)
+        expect(parsed_response).to include(asset_id: "45678")
+      end
+    end
+
+    it "errors if the Accept header is not application/json" do
+      post_multipart "/assets", {
+        asset: {
+          file: fixture_file_upload("asset.txt", "text/plain"),
+        },
+      }, { "HTTP_ACCEPT" => "text/plain" }
+      expect(response.status).to eq(406)
+    end
+
+    it "errors if the Content-Type header is not multipart/form-data" do
+      post "/assets", params: {}, headers: {
+        "CONTENT_TYPE" => "application/json",
+        "HTTP_ACCEPT" => "application/json",
+        "HTTP_AUTHORIZATION" => "Bearer 12345678",
+      }
+
+      expect(response.status).to eq(415)
     end
   end
 end
