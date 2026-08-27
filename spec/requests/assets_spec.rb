uk-gov-mirror/alphagov.hmrc-manuals-api

Add empty endpoint to update existing asset
Functionality will be added in subsequent commits

spec/requests/assets_spec.rb
@@ -229,6 +229,18 @@ describe "assets resource" do
     end
   end
 
+  describe "PUT /asset/:id" do
+    subject do
+      put_multipart "/assets/123456", {}
+    end
+
+    it "responds with ok" do
+      subject
+
+      expect(response.status).to eq(200)
+    end
+  end
+
   describe "POST /assets/:id/regenerate-access" do
     let(:asset_manager_response) do
       {

alphagov/hmrc-manuals-api

spec/requests/assets_spec.rb
@@ -358,6 +358,18 @@ describe "assets resource" do
     end
   end
 
+  describe "PUT /assets/:id" do
+    subject do
+      put_multipart "/assets/#{asset_id}", {}
+    end
+
+    it "responds with ok" do
+      subject
+
+      expect(response.status).to eq(200)
+    end
+  end
+
   describe "POST /assets/:id/regenerate-access" do
     let(:stub_asset_manager_request) { stub_asset_manager_update_asset(asset_id, asset_manager_response.deep_stringify_keys) }
 